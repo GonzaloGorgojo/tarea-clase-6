9fe0d0c0c2ec77305f6ee0e0f3c7484421c2fa41
Gonzalo Gorgojo <gongorgojo@gmail.com>
2021-01-01 23:23:43 -0300

    todo el proyecto

---
+++ tarea1.js
@@ -0,0 +1,102 @@
+const $botonGrupoFamiliar = document.querySelector("#envioGrupo");
+const $grupoFamiliar = document.querySelector("#grupoFamiliar");
+const $formulario = document.querySelector("#formularioFamiliares")
+const $contenedorInputAgregado = document.querySelector("#contenedorInputAgregado");
+const $botonCalcularEdades = document.querySelector("#botonCalcularEdades");
+const $botonBorrarDatos = document.querySelector("#borrarDatos");
+const $mayorEdad = document.querySelector("#mayorEdad");
+const $menorEdad = document.querySelector("#menorEdad");
+const $edadPromedio = document.querySelector("#edadPromedio");
+
+let numeroFamiliar = 0;
+let familiarMayor = 0;
+
+function crearFamiliar(numero) {
+    let nuevoInput = document.createElement("input");
+    nuevoInput.setAttribute("type", "number");
+    nuevoInput.setAttribute("id", "inputEdades");
+    const inputEdades = document.querySelector("#inputEdades");
+    let nuevoLabel = document.createElement("label");
+    let textoLabel = document.createTextNode(`Edad Familiar: ${numero + 1}`)
+
+    nuevoLabel.appendChild(textoLabel)
+    $contenedorInputAgregado.appendChild(nuevoLabel)
+    $contenedorInputAgregado.appendChild(nuevoInput)
+    $botonCalcularEdades.style.display = "block";
+}
+
+
+$botonGrupoFamiliar.onclick = function () {
+    numeroFamiliar = $grupoFamiliar.value;
+
+    if (numeroFamiliar <= 0) {
+        alert('Debe ingresar un numero mayor a 0')
+    } else {
+        for (let i = 0; i < numeroFamiliar; i++) {
+            crearFamiliar(i);
+        }
+        $botonGrupoFamiliar.disabled = true;
+    }
+
+    return false;
+}
+
+$botonCalcularEdades.onclick = function () {
+    edadPromedio();
+    $botonBorrarDatos.style.display = "block";
+    return false;
+}
+
+function edadMayor() {
+    for (let i = 0; i < inputEdades.length; i++) {
+        if (familiarMayor < inputEdades[i].value) {
+            familiarMayor = Number(inputEdades[i].value);
+        }
+    }
+    $mayorEdad.innerHTML = `La Edad mayor es ${familiarMayor}`;
+}
+
+function edadMenor() {
+    let familiarMenor = inputEdades[0];
+
+    for (let j = 0; j < inputEdades.length; j++) {
+        if (familiarMenor > inputEdades[j].value) {
+            familiarMenor = Number(inputEdades[j].value);
+        }
+    }
+    $menorEdad.innerHTML = `La Edad Menor es ${familiarMenor}`;
+}
+
+
+
+function edadPromedio() {
+    let lista = []
+    for (let i = 0; i < inputEdades.length; i++) {
+        lista.push(Number(inputEdades[i].value));
+    }
+    let total = 0;
+    for (let j = 0; j < lista.length; j++) {
+        total = total + lista[j];
+    }
+    promedio = total / lista.length;
+
+    let numero = lista[0];
+    for (let k = 0; k < lista.length; k++) {
+        if (numero > lista[k]) {
+            numero = lista[k];
+        }
+    }
+    if (numero <= 0) {
+        alert('No puede haber edades negativas o 0')
+    } else {
+        edadMenor();
+        edadMayor();
+        $edadPromedio.innerHTML = `La Edad Promedio es ${promedio}`
+        $botonCalcularEdades.disabled = true;
+    }
+}
+$botonBorrarDatos.onclick = function () {
+    location.reload();
+}
+
+
---
+++ tarea2.js
@@ -0,0 +1,121 @@
+const $botonEnvio = document.querySelector("#envioGrupo");
+const $botonCalcular = document.querySelector("#botonCalcular");
+const $botonReiniciar = document.querySelector("#botonReiniciar");
+const $cantidadFamiliares = document.querySelector("#salarioFamiliar");
+const $contenedorInputSalarios = document.querySelector("#contenedorSalarioAgregado");
+const $salarioMayor = document.querySelector("#salarioMayor");
+const $salarioMenor = document.querySelector("#salarioMenor");
+const $anualPromedio = document.querySelector("#anualPromedio");
+const $mensualPromedio = document.querySelector("#mensualPromedio");
+
+let numeroFamiliares = 0;
+let salarioAnualMayor = 0;
+
+
+function crearInputSalarios(numero) {
+    let nuevoInput = document.createElement("input");
+    nuevoInput.setAttribute("type", "number");
+    nuevoInput.setAttribute("id", "inputSalarios");
+    const inputSalarios = document.querySelector("#inputSalarios");
+    let nuevoLabel = document.createElement("label");
+    nuevoLabel.setAttribute("id", "labelCreado");
+    const labelCreado = document.querySelector("#labelCreado");
+    let textoLabel = document.createTextNode(`Salario Anual Familiar ${numero + 1}:`)
+    let nuevoBoton = document.createElement("button");
+    let textoBoton = document.createTextNode("Borrar Familiar")
+    nuevoBoton.setAttribute("id", "botonBorrarSalarios");
+
+
+    nuevoLabel.appendChild(textoLabel)
+    $contenedorInputSalarios.appendChild(nuevoLabel)
+    $contenedorInputSalarios.appendChild(nuevoInput)
+    nuevoBoton.appendChild(textoBoton)
+    $contenedorInputSalarios.appendChild(nuevoBoton)
+
+    $botonCalcular.style.display = "block";
+}
+
+$botonEnvio.onclick = function () {
+    numeroFamiliares = $cantidadFamiliares.value;
+
+    if (numeroFamiliares <= 0) {
+        alert('Debe ingresar un numero mayor a 0')
+    } else {
+        for (let i = 0; i < numeroFamiliares; i++) {
+            crearInputSalarios(i);
+            borrarSalarios(i);
+        }
+        $botonEnvio.disabled = true;
+    }
+
+    return false;
+}
+
+
+function borrarSalarios(n) {
+    const $botonBorrarSalarios = document.querySelectorAll("#botonBorrarSalarios");
+
+    $botonBorrarSalarios[n].onclick = function () {
+        $contenedorInputSalarios.removeChild(labelCreado[n]);
+        $contenedorInputSalarios.removeChild(inputSalarios[n]);
+        $contenedorInputSalarios.removeChild($botonBorrarSalarios[n]);
+        return false;
+    }
+
+}
+
+$botonCalcular.onclick = function(){
+    calcularSalarios();
+    $botonReiniciar.style.display='block';
+    $botonCalcular.disabled=true;
+    return false;
+}
+$botonReiniciar.onclick = function () {
+    location.reload();
+}
+
+function calcularSalarios(){
+    salarioMayor();
+    salarioMenor();
+    salarioAnualPromedio();
+    salarioMensualPromedio();
+}
+
+function salarioMayor(){
+    for(let x = 0; x < inputSalarios.length; x++) {
+        if(salarioAnualMayor < inputSalarios[x].value){
+            salarioAnualMayor = Number(inputSalarios[x].value);
+        }
+    }
+    $salarioMayor.innerHTML = `El Salario Anual Mayor de su Familia es ${salarioAnualMayor}`
+}
+    
+function salarioMenor() {
+    let salarioAnualMenor = inputSalarios[0].value;
+
+    for(let x= 0;x < inputSalarios.length; x++){
+        if(salarioAnualMenor > inputSalarios[x].value){
+            salarioAnualMenor = Number(inputSalarios[x].value);
+        }
+    }
+    $salarioMenor.innerHTML = `El Salario Anual Menor de su Familia es ${salarioAnualMenor}`
+}
+
+function salarioAnualPromedio() {
+    let total = 0;
+   for (let x=0; x < inputSalarios.length; x++) {
+    total = total + Number(inputSalarios[x].value);
+   }
+   let promedio = total / inputSalarios.length;
+   $anualPromedio.innerHTML = `El Salario Anual Promedio de su Familia es ${promedio}`
+
+}
+
+function salarioMensualPromedio() {
+    let total = 0;
+   for (let x=0; x < inputSalarios.length; x++) {
+    total = total + Number(inputSalarios[x].value);
+   }
+   let mensualPromedio = total / 12 / inputSalarios.length;
+   $mensualPromedio.innerHTML = `El Salario Mensual Promedio de su Familia es ${mensualPromedio.toFixed(2)}`
+}
